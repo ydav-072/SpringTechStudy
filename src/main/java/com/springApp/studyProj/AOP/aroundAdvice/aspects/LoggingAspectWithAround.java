5d64f4c78c63788c9a1c6bc76167da046a67bc24
ydav-072 <ydav-07@mail.ru>
2021-08-23 16:14:49 +0300

    some fix

--- src/main/java/com/springApp/studyProj/AOP/aroundAdvice/aspects/LoggingAspectWithAround.java
+++ src/main/java/com/springApp/studyProj/AOP/aroundAdvice/aspects/LoggingAspectWithAround.java
@@ -3,8 +3,6 @@ package com.springApp.studyProj.AOP.aroundAdvice.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
